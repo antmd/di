da2c22cf1c386a4ab24886d1ea5bb492aef7b0a7
Krzysztof Jusiak <krzysztof@jusiak.net>
2015-04-15 20:31:12 +0100

    more fixes

--- example/extensions/contextual.cpp
+++ example/extensions/contextual.cpp
@@ -47,7 +47,6 @@ public:
     auto policies() noexcept {
         return di::make_policies([&](auto type, auto dependency, auto... ctor) {
             using arg = typename decltype(type)::type;
-            using given = typename decltype(dependency)::given;
             types[v[i - 1]].push_back(&typeid(arg));
             auto ctor_size = sizeof...(ctor);
             while (ctor_size--) {
@@ -77,7 +76,7 @@ struct type_<di::aux::type_list<Ts...>> {
 
 template<class T>
 struct type {
-    auto operator ->() const {
+    auto operator->() const {
         return std::make_unique<type_<di::aux::type_list<T>>>();
     }
 
@@ -88,49 +87,70 @@ struct type {
 
 struct context_ {
     template<class T>
-    auto operator=(std::unique_ptr<T> t) const {
-        return *t;
+    auto operator=(std::unique_ptr<T> object) const {
+        return *object;
     }
 
     template<class T>
-    auto operator=(T t) const {
-        return t;
+    auto operator=(T object) const {
+        return object;
     }
 };
 
 context_ context;
 
+template<class T, class R>
+struct all {
+    using type = T;
+    T object;
+};
+
+template<class R>
 struct then_ {
     template<class T>
-    T then(T) {
-        return {};
+    auto then(T value) {
+        return all<T, R>{value};
     }
 };
 
 template<class T>
-then_ when(T&&) {
+then_<T> when(T&&) {
     return {};
 }
 
+template<class TResult>
 struct contextual {
     template<class... Ts>
-    contextual(Ts&&...) {
+    explicit contextual(Ts&&... args)
+        : types{std::make_pair(&typeid(typename Ts::type), args.object)...}
+    { }
+
+    template<class... TCtor>
+    using make_type_list = di::aux::type_list<typename get_type<TCtor>::type...>;
+
+    template<class TInjector>
+    auto operator()(const TInjector& injector) const -> TResult {
+        const auto& types = injector.types;
+        //const auto it = types.find(&typeid(make_type_list<TCtor...>));
+        //std::cout << typeid(make_type_list<TCtor...>).name() << std::endl;
+        //if (it != types.end()) {
+            //std::cout << "dupa: " << it->second << std::endl;
+            //return it->second;
+        //}
+
+        return 42;
     }
 
-
-    template<class T, class TDependency, class... TCtor>
-    auto operator()(T&&, TDependency&&, TCtor&&...) const {
-        return 0;
-    }
+    std::map<const std::type_info*, TResult> types;
 };
 
 int main() {
     auto injector = di::make_injector<build_types>(
         di::bind<int>.to(
-            contextual{
+            contextual<int>{
                 when(context = type<a>()).then(1)
-              , when(context = type<a>() -> type<b>()).then(2)
-              , when(context = type<a>() -> type<b>() -> type<c>()).then(3)
+              //, when(context = type<a>() -> type<b>()).then(2)
+              //, when(context = type<a>() -> type<b>() -> type<c>()).then(3)
             }
         )
     );
